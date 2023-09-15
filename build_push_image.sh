b4f0322433e61f8f2c58a2d0970444b63ea60926
Rohmat Sunaryo <rohmat.sunaryo@gmail.com>
2023-09-15 07:42:22 +0700

    Delapan

--- build_push_image.sh
+++ build_push_image.sh
@@ -6,7 +6,6 @@ docker images
 # merubah nama image agar sesuai dengan format Docker Hub
 docker tag item-app:v1 matnaryo/item-app:v1
 # login ke akun Docker Hub
-export PASSWORD_DOCKER_HUB=dicoding123
 echo $PASSWORD_DOCKER_HUB | docker login -u matnaryo --password-stdin
 # push image ke Docker Hub
 docker push matnaryo/item-app:v1
