9dded448f7423c92dc1d0309f5d5204d211289e3
Rohmat Sunaryo <rohmat.sunaryo@gmail.com>
2023-10-15 19:36:44 +0700

    Rumah

--- build_push_image_karsajobs_ui.sh
+++ build_push_image_karsajobs_ui.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 # membuat Docker image dari Dockerfile
 docker build -t matnaryo/karsajobs-ui:latest .
+# melihat daftar image di lokal
+docker images
+# merubah nama image agar sesuai dengan format Docker Hub
+docker tag matnaryo/karsajobs-ui:latest matnaryo/karsajobs-ui:latest
 # login ke akun Docker Hub
 echo $PASSWORD_DOCKER_HUB | docker login -u matnaryo --password-stdin
 # push image ke Docker Hub
