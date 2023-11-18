4c3fa1fcdf10c079412b10ae65c3ac398d991de0
Rohmat Sunaryo <rohmat.sunaryo@gmail.com>
2023-11-19 06:20:10 +0700

    19/11

--- build_push_image.sh
+++ build_push_image.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # membuat Docker image dari Dockerfile
-docker build -t order-app:v1 .
+#docker build -t order-app:v2 .
 # melihat daftar image di lokal
 docker images
 # merubah nama image agar sesuai dengan format Docker Hub
-docker tag order-app:v1 matnaryo/order-app:v1
+docker tag order-app:v2 matnaryo/order-app:v2
 # login ke akun Docker Hub
-echo $PASSWORD_DOCKER_HUB | docker login -u matnaryo --password-stdin
+#echo $PASSWORD_DOCKER_HUB | docker login -u matnaryo --password-stdin
 # push image ke Docker Hub
-docker push matnaryo/order-app:v1
+docker push matnaryo/order-app:v2
